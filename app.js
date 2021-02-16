cdf56e9fe5fa2357099e9689fe0e6dc6b2bb1752
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-02-16 08:29:40 -0300

    I added validations

--- app.js
+++ app.js
@@ -3,25 +3,49 @@ const app = express();
 app.use(express.json()); // Para que a API receba dados no formato JSON
 
 const contatos = ["Huguinho", "Zezinho", "Luizinho", "Zequinha"];
+/*
+app.use((req, res, next) => {
+  console.log('Acessou o middleware');
+  next();
+});
+*/
+
+function valContato(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "Necessário enviar o nome!"
+    })
+  }
+  return next();
+}
+
+function valPosContato(req, res, next) {
+  if (!contatos[req.params.id]) {
+    return res.status(400).json({
+      error: "Contato não encontrado"
+    })
+  }
+  return next();
+}
 
 app.get('/', (req, res) => {
   res.json(contatos);
 });
 
-app.get('/contatos/:id', (req, res) => {
+app.get('/contatos/:id', valPosContato, (req, res) => {
   const { id } = req.params;
   return res.json({ 
     name: contatos[id], 
   });
 });
 
-app.post('/contatos', (req, res) => {
+app.post('/contatos', valContato, (req, res) => {
   const { name } = req.body;
   contatos.push(name);
   return res.json(contatos);
 });
 
-app.put('/contatos/:id', (req, res) => {
+app.put('/contatos/:id', valPosContato, (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
   contatos[id] = name;
@@ -29,7 +53,7 @@ app.put('/contatos/:id', (req, res) => {
 
 });
 
-app.delete('/contatos/:id', (req, res) => {
+app.delete('/contatos/:id', valPosContato, (req, res) => {
   const { id } = req.params;
   contatos.splice(id, 1);
   return res.json(contatos);
